aaa2b71de50f91a932b68cbf5eaddcda72066e55
Link4Electronics <link4electronics@gmail.com>
2026-02-06 17:27:34 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 set -eu
-
 ARCH=$(uname -m)
-export ARCH
+VERSION=$(pacman -Q openlara-git | awk '{print $2; exit}') # example command to get version of application here
+export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/pixmaps/OpenLara.png
+export ICON=https://raw.githubusercontent.com/XProger/OpenLara/refs/heads/master/src/platform/psv/sce_sys/icon0.png
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/OpenLara
+quick-sharun /usr/bin/openlara
 
 # Additional changes can be done in between here
 
