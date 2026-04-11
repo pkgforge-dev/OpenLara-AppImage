0a7e9509e24bfa28c8d1c90d78e493a0eadfec26
Link4Electronics <link4electronics@gmail.com>
2026-04-11 19:38:08 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -2,19 +2,20 @@
 
 set -eu
 ARCH=$(uname -m)
-VERSION=$(pacman -Q openlara-git | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/XProger/OpenLara/refs/heads/master/src/platform/psv/sce_sys/icon0.png
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/openlara
-
-# Additional changes can be done in between here
+quick-sharun ./AppDir/bin/openlara
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage
