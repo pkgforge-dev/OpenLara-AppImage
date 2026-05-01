5bd851d192f4668c4c84e586193733ae41271301
Link4Electronics <link4electronics@gmail.com>
2026-05-01 08:01:52 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,15 +6,20 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-    clang    \
-    libdecor
+pacman -Syu --noconfirm sdl3
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-make-aur-package openlara-git
+echo "Building OpenLara..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/XProger/OpenLara"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --depth 1 "$REPO" ./OpenLara
+echo "$VERSION" > ~/version
 
-# If the application needs to be manually built that has to be done down here
+mkdir -p ./AppDir/bin
+cd ./OpenLara/src/platform/sdl3
+make -j$(nproc)
+mv -v openlara ../../../../AppDir/bin
