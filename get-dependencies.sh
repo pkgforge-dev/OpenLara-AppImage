d128d57682a205d873ff010739011ba2ebe70744
Link4Electronics <link4electronics@gmail.com>
2026-05-05 10:55:29 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,18 +7,14 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    clang    \
-    libdecor
+    libdecor \
+    sdl3
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-if [ "$ARCH" = "x86_64" ]; then
-    make-aur-package openlara-git
-else
-    PRE_BUILD_CMDS='sed -i "\|strip ../../../bin/OpenLara|d" ./openlara-git/src/platform/nix/build.sh' make-aur-package openlara-git
-fi
+#make-aur-package PACKAGENAME
 
 # If the application needs to be manually built that has to be done down here
