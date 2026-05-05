ca6c2e017647c9d02ba5116f60d204c05dc7cf88
Link4Electronics <link4electronics@gmail.com>
2026-05-05 10:54:24 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -12,9 +12,13 @@ export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/openlara
+quick-sharun ./AppDir/bin/openlara
 
 # Additional changes can be done in between here
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the test fails due to the app
+# having issues running in the CI use --simple-test instead
+quick-sharun --simple-test ./dist/*.AppImage
