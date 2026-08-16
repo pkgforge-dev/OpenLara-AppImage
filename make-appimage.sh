c933d7226ef2a6b82c51dd4ae4226a204825f854
Link4Electronics <link4electronics@gmail.com>
2026-08-15 21:18:02 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -13,8 +13,6 @@ export DEPLOY_VULKAN=1
 # Deploy dependencies
 quick-sharun ./AppDir/bin/openlara
 
-# Additional changes can be done in between here
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
